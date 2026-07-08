bc430950f3aa029b576ab90a83d8f4d64a1bb850
addd63813-sudo <addd63813@gmail.com>
2026-07-08 18:07:19 +0900

    Update server message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.set("trust proxy", true);
 
-// Render 환경 변수에서 Firebase 키 읽기
+// Firebase 서비스 계정
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 // Firebase 초기화
@@ -18,24 +18,25 @@ const db = getFirestore();
 
 app.get("/", async (req, res) => {
   try {
-    // 실제 접속자 IP만 가져오기
+    // 실제 접속 IP 가져오기
     const forwarded = req.headers["x-forwarded-for"];
     const ip = forwarded
       ? forwarded.split(",")[0].trim()
       : req.socket.remoteAddress;
 
-    // Firestore에 저장
+    // Firestore 저장
     await db.collection("access").add({
-      ip,
+      ip: ip,
       time: new Date().toLocaleString("ko-KR", {
         timeZone: "Asia/Seoul",
       }),
     });
 
-    res.send("접속 기록 저장 완료");
+    // 사용자에게 보여줄 문구
+    res.status(500).send("에러: 클라이언트를 닫고 다시 시도하세요.");
   } catch (err) {
     console.error(err);
-    res.status(500).send("오류 발생");
+    res.status(500).send("에러: 클라이언트를 닫고 다시 시도하세요.");
   }
 });
 
